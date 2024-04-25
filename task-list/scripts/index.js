90f39d14ea32660663aaded5e67a714cb941e47d
Christopher Edwards <66543275+chedwards492@users.noreply.github.com>
2024-04-25 13:39:30 -0700

    Working JSON file read and tests

--- task-list/scripts/index.js
+++ task-list/scripts/index.js
@@ -2,12 +2,12 @@
  * Adds JSON data to the task list
  * @Returns void
  */
-async function addJSONData() {
+function addJSONData() {
   // find taskList and create new task
-  const taskList = document.querySelector(".task-list-grid");
-  const newTask = document.createElement("div");
-  newTask.setAttribute("class", "task");
-  taskList.appendChild(newTask);
+  const taskList = document.querySelector(".task-list-flex");
+  // const newTask = document.createElement("div");
+  // newTask.setAttribute("class", "task");
+  // taskList.appendChild(newTask);
   // fetch JSON data
   fetch("../data/tasks.json").then((ret) => {
     return ret.json();
@@ -16,21 +16,38 @@ async function addJSONData() {
     const tasks = data.tasks;
 
     tasks.forEach(function (task) {
-      newTask.insertAdjacentHTML("beforeend",
-        `<div class="task">
+      if ((task.name == null) || (task.deadline == null) ||
+          (task.name.length == 0) || (task.deadline.length == 0)) { 
+        console.error("Incorrect format for JSON task");
+      } else {
+        const newTask = document.createElement("div");
+        newTask.setAttribute("class", "task");
+        taskList.appendChild(newTask);
+        newTask.insertAdjacentHTML("afterbegin",
+          `
+          <input type="checkbox" id="toggle-btn">
+          <label for="toggle-btn">Mark as Complete</label>
           <p>${task.name}</p>
           <p>Due: ${task.deadline}</p>
-        </div>`
-      );
+          <button type="delete">Delete</button>
+          `
+        );
+      }
     });
   }
-  );
+  ).catch(error => {
+    console.error("Error fetching JSON data: ", error);
+  });
 }
 
 addJSONData();
 
 // html has two boxes, one for deadline, and one for task?
-async function addTask() {
+/**
+ * 
+ * @returns 
+ */
+function addTask() {
   // find taskList and create new task
   const taskName = document.getElementById("task-name");
   const taskDeadline = document.getElementById("deadline");
